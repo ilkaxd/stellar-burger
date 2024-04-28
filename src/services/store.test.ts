6a9a6c7e5e8f54e05e366eb5b9e5158b1070e459
ilka.xd <ilka.xd@yandex.ru>
2024-04-28 11:48:15 +0300

    after first review

--- src/services/store.test.ts
+++ src/services/store.test.ts
@@ -1,47 +1,101 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { rootReducer } from './store';
-import burgerConstructorReducer from './slices/burger-constructor/slice';
-import feedReducer from './slices/feed/slice';
-import ingredientsReducer from './slices/ingredients/slice';
-import orderReducer from './slices/order/slice';
-import userReducer from './slices/user/slice';
+import burgerConstructorReducer, {
+  burgerConstructorState
+} from './slices/burger-constructor/slice';
+import feedReducer, { FeedState } from './slices/feed/slice';
+import ingredientsReducer, {
+  IngredientsState
+} from './slices/ingredients/slice';
+import orderReducer, { OrderState } from './slices/order/slice';
+import userReducer, { UserState } from './slices/user/slice';
 
 describe('Проверяют правильную инициализацию rootReducer', () => {
-  const store = configureStore({ reducer: rootReducer });
+  const burgerConstructorInitialState: burgerConstructorState = {
+    burgerConstructor: {
+      bun: null,
+      ingredients: []
+    },
+    error: null
+  };
+
+  const feedInitialState: FeedState = {
+    orders: [],
+    isFeedsLoading: false,
+    order: null,
+    isOrderLoading: false,
+    total: 0,
+    totalToday: 0,
+    error: null
+  };
+
+  const ingredientsInitialState: IngredientsState = {
+    ingredients: [],
+    isIngredientsLoading: false,
+    error: null
+  };
+
+  const orderInitialState: OrderState = {
+    order: null,
+    isOrderLoading: false,
+    error: null
+  };
+
+  const userInitialState: UserState = {
+    isAuthenticated: false,
+    loginUserRequest: false,
+    user: null,
+    orders: [],
+    ordersRequest: false,
+    error: null
+  };
+
+  const store = configureStore({
+    reducer: rootReducer,
+    preloadedState: {
+      burgerConstructor: burgerConstructorInitialState,
+      feed: feedInitialState,
+      ingredients: ingredientsInitialState,
+      order: orderInitialState,
+      user: userInitialState
+    }
+  });
 
   test('smoke test burger constructor', () => {
     expect(store.getState().burgerConstructor).toEqual(
-      burgerConstructorReducer(undefined, { type: 'UNKNOWN_ACTION' })
+      burgerConstructorReducer(burgerConstructorInitialState, {
+        type: 'UNKNOWN_ACTION'
+      })
     );
 
     const addIngredientAction = { type: 'addIngredient' };
     store.dispatch(addIngredientAction);
     expect(store.getState().burgerConstructor).toEqual(
-      burgerConstructorReducer(undefined, addIngredientAction)
+      burgerConstructorInitialState
     );
 
     const upIngredientAction = { type: 'upIngredient' };
     store.dispatch(upIngredientAction);
     expect(store.getState().burgerConstructor).toEqual(
-      burgerConstructorReducer(undefined, upIngredientAction)
+      burgerConstructorInitialState
     );
 
     const downIngredientAction = { type: 'downIngredient' };
     store.dispatch(downIngredientAction);
     expect(store.getState().burgerConstructor).toEqual(
-      burgerConstructorReducer(undefined, downIngredientAction)
+      burgerConstructorInitialState
     );
 
     const removeIngredientAction = { type: 'removeIngredient' };
     store.dispatch(removeIngredientAction);
     expect(store.getState().burgerConstructor).toEqual(
-      burgerConstructorReducer(undefined, removeIngredientAction)
+      burgerConstructorInitialState
     );
 
     const clearBurgerConstructorAction = { type: 'clearBurgerConstructor' };
     store.dispatch(clearBurgerConstructorAction);
     expect(store.getState().burgerConstructor).toEqual(
-      burgerConstructorReducer(undefined, clearBurgerConstructorAction)
+      burgerConstructorInitialState
     );
   });
 
@@ -52,15 +106,11 @@ describe('Проверяют правильную инициализацию roo
 
     const getFeedsThunkAction = { type: 'getFeedsThunk' };
     store.dispatch(getFeedsThunkAction);
-    expect(store.getState().feed).toEqual(
-      feedReducer(undefined, getFeedsThunkAction)
-    );
+    expect(store.getState().feed).toEqual(feedInitialState);
 
     const getOrderByNumberThunkAction = { type: 'getOrderByNumberThunk' };
     store.dispatch(getOrderByNumberThunkAction);
-    expect(store.getState().feed).toEqual(
-      feedReducer(undefined, getOrderByNumberThunkAction)
-    );
+    expect(store.getState().feed).toEqual(feedInitialState);
   });
 
   test('smoke test ingredients', () => {
@@ -70,9 +120,7 @@ describe('Проверяют правильную инициализацию roo
 
     const getIngredientsThunkAction = { type: 'getIngredientsThunk' };
     store.dispatch(getIngredientsThunkAction);
-    expect(store.getState().ingredients).toEqual(
-      ingredientsReducer(undefined, getIngredientsThunkAction)
-    );
+    expect(store.getState().ingredients).toEqual(ingredientsInitialState);
   });
 
   test('smoke test order', () => {
@@ -82,15 +130,11 @@ describe('Проверяют правильную инициализацию roo
 
     const clearOrderAction = { type: 'clearOrder' };
     store.dispatch(clearOrderAction);
-    expect(store.getState().order).toEqual(
-      orderReducer(undefined, clearOrderAction)
-    );
+    expect(store.getState().order).toEqual(orderInitialState);
 
     const orderBurgerThunkAction = { type: 'orderBurgerThunk' };
     store.dispatch(orderBurgerThunkAction);
-    expect(store.getState().order).toEqual(
-      orderReducer(undefined, orderBurgerThunkAction)
-    );
+    expect(store.getState().order).toEqual(orderInitialState);
   });
 
   test('smoke test user', () => {
@@ -100,38 +144,26 @@ describe('Проверяют правильную инициализацию roo
 
     const clearErrorsAction = { type: 'clearErrors' };
     store.dispatch(clearErrorsAction);
-    expect(store.getState().user).toEqual(
-      userReducer(undefined, clearErrorsAction)
-    );
+    expect(store.getState().user).toEqual(userInitialState);
 
     const loginUserThunkAction = { type: 'loginUserThunk' };
     store.dispatch(loginUserThunkAction);
-    expect(store.getState().user).toEqual(
-      userReducer(undefined, loginUserThunkAction)
-    );
+    expect(store.getState().user).toEqual(userInitialState);
 
     const logoutUserThunkAction = { type: 'logoutUserThunk' };
     store.dispatch(logoutUserThunkAction);
-    expect(store.getState().user).toEqual(
-      userReducer(undefined, logoutUserThunkAction)
-    );
+    expect(store.getState().user).toEqual(userInitialState);
 
     const getUserThunkAction = { type: 'getUserThunk' };
     store.dispatch(getUserThunkAction);
-    expect(store.getState().user).toEqual(
-      userReducer(undefined, getUserThunkAction)
-    );
+    expect(store.getState().user).toEqual(userInitialState);
 
     const updateUserThunkAction = { type: 'updateUserThunk' };
     store.dispatch(updateUserThunkAction);
-    expect(store.getState().user).toEqual(
-      userReducer(undefined, updateUserThunkAction)
-    );
+    expect(store.getState().user).toEqual(userInitialState);
 
     const getOrdersThunkAction = { type: 'getOrdersThunk' };
     store.dispatch(getOrdersThunkAction);
-    expect(store.getState().user).toEqual(
-      userReducer(undefined, getOrdersThunkAction)
-    );
+    expect(store.getState().user).toEqual(userInitialState);
   });
 });
